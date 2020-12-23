0b3ed4fb8eb3b9eac8ce6f4ede16ca68ea1444ba
Ajit Mourya <mouryaa33@gmail.com>
2020-12-24 00:20:57 +0530

    django-azure-backup

---
+++ README.md
@@ -0,0 +1,14 @@
+# Glib Backup
+
+This package can take postgres dump and able to upload data in azure cloud storage.
+
+## Build Process
+open the link.
+[click here for steps.](https://packaging.python.org/tutorials/packaging-projects/#uploading-the-distribution-archives)
+
+## Required 
+1. Django Default Database Config.
+2. AZURE_ACCOUNT_NAME (Azure Storage Account Name).
+3. AZURE_CREDENTIAL (Azure Key).
+4. AZURE_CONTAINER (Azure Container Name).
+5. AZURE_BACKUP_FOLDER_NAME.
---
+++ main_backup/urls.py
@@ -0,0 +1,22 @@
+"""rewaflux URL Configuration
+
+The `urlpatterns` list routes URLs to views1. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views1
+    1. Add an import:  from my_app import views1
+    2. Add a URL to urlpatterns:  path('', views1.home, name='home')
+Class-based views1
+    1. Add an import:  from other_app.views1 import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include
+from django.conf import settings
+
+urlpatterns = [
+]
